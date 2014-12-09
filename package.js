babc94e84f8b1c328779841660b01740858240a6
Franco Catena <francocatena@gmail.com>
2014-12-09 00:32:34 -0300

    Version 0.2.1 bump...

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type for AutoForm',
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/francocatena/meteor-autoform-bs-datetimepicker.git'
 })
 
