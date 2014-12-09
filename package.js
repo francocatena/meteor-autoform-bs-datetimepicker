17ce337a89164f1fd2873ae7cf6b0ce46c5e7aeb
Franco Catena <francocatena@gmail.com>
2014-12-09 00:35:42 -0300

    Depend on momentjs (version 0.2.2 bump)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type for AutoForm',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/francocatena/meteor-autoform-bs-datetimepicker.git'
 })
 
@@ -9,6 +9,8 @@ Package.onUse(function (api) {
   api.use('templating@1.0.0')
   api.use('blaze@2.0.0')
   api.use('aldeed:autoform@4.0.0')
+  api.use('mrt:moment@2.8.1')
+
   api.addFiles([
     'autoform-bs-datetimepicker.html',
     'autoform-bs-datetimepicker.js'
