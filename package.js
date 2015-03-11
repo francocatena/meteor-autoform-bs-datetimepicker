a24526723480723e642bc63056d2fe310ba5a6d9
Franco Catena <francocatena@gmail.com>
2015-03-11 09:12:40 -0300

    Use AutoForm 5.0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'francocatena:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type for AutoForm',
-  version: '1.0.0',
+  version: '1.0.2',
   git: 'https://github.com/francocatena/meteor-autoform-bs-datetimepicker.git'
 })
 
 Package.onUse(function (api) {
   api.use('templating@1.0.0')
   api.use('blaze@2.0.0')
-  api.use('aldeed:autoform@4.0.0')
+  api.use('aldeed:autoform@5.0.0')
   api.use('momentjs:moment@2.9.0')
 
   api.addFiles([
